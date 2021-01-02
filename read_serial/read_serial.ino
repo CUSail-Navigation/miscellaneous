781487406fce6ac6d9487f062acab4cb99925959
Courtney McBeth <clm325@cornell.edu>
2021-01-01 23:02:14 -0500

    added arduino serial reader for rpi uart tests

---
+++ read_serial/read_serial.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial1.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial1.available()) {
+    Serial.println(Serial1.readString());
+  }
+}
